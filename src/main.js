a30d9b62517a7107086dfe3f83cd5a6f914d9e5f
Jackson <deltarax@gmail.com>
2022-04-15 13:58:08 -0700

    Added name and point breakdown

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+// Created by Jackson Gerard 4/15/2022
+// This took roughly 5 hours - didn't run into any jarring bugs
+
+// Use Phaser's particle emitter to create a particle explosion when the rocket hits the spaceship (20)
+// Implement a new timing/scoring mechanism that adds time to the clock for successful hits (20)
+// Create a new spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (20)
+// Implement parallax scrolling (10)
+// Create a new animated sprite for the Spaceship enemies (10) (Look at the fire!)
+// Display the time remaining (in seconds) on the screen (10)
+// Implement the speed increase that happens after 30 seconds in the original game (5)
+// Allow the player to control the Rocket after it's fired (5) (Only on Novice mode)
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
